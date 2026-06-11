e92d34a5eb3415d1b3215d8913568f8a3f68e77c
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2026-06-10 20:52:25 -0600

    add: week 01 Day03 Demo01: Window form
    Form events:
    Load, Closing, Closed, Paint
    Controls:
    Button, text box, label

---
+++ Week01Day03Demo01WindowForm/Week01Day03Demo01WindowForm/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Week01Day03Demo01WindowForm
+{
+    partial class UI_Form_Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_Login_btn = new System.Windows.Forms.Button();
+            this.UI_Display_Lbl = new System.Windows.Forms.Label();
+            this.UI_Input_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_Input1_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_Input2_Tbx = new System.Windows.Forms.TextBox();
+            this.UI_Sum_Btn = new System.Windows.Forms.Button();
+            this.UI_Screen_Lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UI_Login_btn
+            // 
+            this.UI_Login_btn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.UI_Login_btn.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_Login_btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UI_Login_btn.Location = new System.Drawing.Point(215, 98);
+            this.UI_Login_btn.Name = "UI_Login_btn";
+            this.UI_Login_btn.Padding = new System.Windows.Forms.Padding(5);
+            this.UI_Login_btn.Size = new System.Drawing.Size(209, 68);
+            this.UI_Login_btn.TabIndex = 0;
+            this.UI_Login_btn.Text = "Transfer Text";
+            this.UI_Login_btn.UseVisualStyleBackColor = false;
+            this.UI_Login_btn.Click += new System.EventHandler(this.UI_Login_btn_Click);
+            // 
+            // UI_Display_Lbl
+            // 
+            this.UI_Display_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_Display_Lbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.UI_Display_Lbl.Location = new System.Drawing.Point(36, 27);
+            this.UI_Display_Lbl.Name = "UI_Display_Lbl";
+            this.UI_Display_Lbl.Padding = new System.Windows.Forms.Padding(10);
+            this.UI_Display_Lbl.Size = new System.Drawing.Size(159, 46);
+            this.UI_Display_Lbl.TabIndex = 1;
+            this.UI_Display_Lbl.Text = "Value";
+            this.UI_Display_Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UI_Input_Tbx
+            // 
+            this.UI_Input_Tbx.Location = new System.Drawing.Point(215, 40);
+            this.UI_Input_Tbx.Name = "UI_Input_Tbx";
+            this.UI_Input_Tbx.Size = new System.Drawing.Size(243, 26);
+            this.UI_Input_Tbx.TabIndex = 2;
+            // 
+            // UI_Input1_Tbx
+            // 
+            this.UI_Input1_Tbx.Location = new System.Drawing.Point(175, 277);
+            this.UI_Input1_Tbx.Name = "UI_Input1_Tbx";
+            this.UI_Input1_Tbx.Size = new System.Drawing.Size(132, 26);
+            this.UI_Input1_Tbx.TabIndex = 3;
+            this.UI_Input1_Tbx.Text = "0";
+            // 
+            // UI_Input2_Tbx
+            // 
+            this.UI_Input2_Tbx.Location = new System.Drawing.Point(362, 277);
+            this.UI_Input2_Tbx.Name = "UI_Input2_Tbx";
+            this.UI_Input2_Tbx.Size = new System.Drawing.Size(132, 26);
+            this.UI_Input2_Tbx.TabIndex = 4;
+            this.UI_Input2_Tbx.Text = "0";
+            // 
+            // UI_Sum_Btn
+            // 
+            this.UI_Sum_Btn.BackColor = System.Drawing.Color.Green;
+            this.UI_Sum_Btn.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_Sum_Btn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UI_Sum_Btn.Location = new System.Drawing.Point(215, 361);
+            this.UI_Sum_Btn.Name = "UI_Sum_Btn";
+            this.UI_Sum_Btn.Padding = new System.Windows.Forms.Padding(5);
+            this.UI_Sum_Btn.Size = new System.Drawing.Size(209, 68);
+            this.UI_Sum_Btn.TabIndex = 5;
+            this.UI_Sum_Btn.Text = "SUM";
+            this.UI_Sum_Btn.UseVisualStyleBackColor = false;
+            this.UI_Sum_Btn.Click += new System.EventHandler(this.UI_Sum_Btn_Click);
+            // 
+            // UI_Screen_Lbl
+            // 
+            this.UI_Screen_Lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UI_Screen_Lbl.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.UI_Screen_Lbl.Location = new System.Drawing.Point(175, 203);
+            this.UI_Screen_Lbl.Name = "UI_Screen_Lbl";
+            this.UI_Screen_Lbl.Padding = new System.Windows.Forms.Padding(10);
+            this.UI_Screen_Lbl.Size = new System.Drawing.Size(319, 46);
+            this.UI_Screen_Lbl.TabIndex = 6;
+            this.UI_Screen_Lbl.Text = "0";
+            this.UI_Screen_Lbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // UI_Form_Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 450);
+            this.Controls.Add(this.UI_Screen_Lbl);
+            this.Controls.Add(this.UI_Sum_Btn);
+            this.Controls.Add(this.UI_Input2_Tbx);
+            this.Controls.Add(this.UI_Input1_Tbx);
+            this.Controls.Add(this.UI_Input_Tbx);
+            this.Controls.Add(this.UI_Display_Lbl);
+            this.Controls.Add(this.UI_Login_btn);
+            this.Name = "UI_Form_Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UI_Form_Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UI_Form_Form1_FormClosed);
+            this.Load += new System.EventHandler(this.UI_Form_Form1_Load);
+            this.Shown += new System.EventHandler(this.UI_Form_Form1_Shown);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.UI_Form_Form1_Paint);
+            this.MouseLeave += new System.EventHandler(this.UI_Form_Form1_MouseLeave);
+            this.MouseHover += new System.EventHandler(this.UI_Form_Form1_MouseHover);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button UI_Login_btn;
+        private System.Windows.Forms.Label UI_Display_Lbl;
+        private System.Windows.Forms.TextBox UI_Input_Tbx;
+        private System.Windows.Forms.TextBox UI_Input1_Tbx;
+        private System.Windows.Forms.TextBox UI_Input2_Tbx;
+        private System.Windows.Forms.Button UI_Sum_Btn;
+        private System.Windows.Forms.Label UI_Screen_Lbl;
+    }
+}
+
---
+++ Week01Day03Demo01WindowForm/Week01Day03Demo01WindowForm/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week01Day03Demo01WindowForm
+{
+    public partial class UI_Form_Form1 : Form
+    {
+        public UI_Form_Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void UI_Form_Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Form has been loaded");
+        }
+
+        private void UI_Form_Form1_Shown(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Form is shown on the screen");
+
+        }
+
+        private void UI_Form_Form1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Paint event occurrued");
+        }
+
+        private void UI_Form_Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine($"Form is closing Reason of closing {e.CloseReason}" );
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                // Preventing the user from closing the form
+                //e.Cancel = true;
+            }
+
+        }
+
+        private void UI_Form_Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Form has been closed");
+        }
+
+        private void UI_Form_Form1_MouseHover(object sender, EventArgs e)
+        {
+            BackColor = Color.LightBlue;
+            // change the title for the fom
+            Text = "Mouse is hovering over the form";
+        }
+
+        private void UI_Form_Form1_MouseLeave(object sender, EventArgs e)
+        {
+            BackColor = Color.LightGray;
+            Text = "Mouse has left";
+        }
+
+        private void UI_Login_btn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Trace.WriteLine("Transfer Text button clicked");
+            // changing the title of the form
+            //this.Text = "Login Form";
+            UI_Login_btn.BackColor = Color.Green;
+
+            /* Take the text from Text Box and show it on label 
+             * Then, empty your text box
+             */
+
+            if (UI_Input_Tbx.Text.ToString().Length > 0)
+            {
+                UI_Input_Tbx.ForeColor = Color.White;
+                UI_Input_Tbx.BackColor = Color.Green;
+                // Transfering text to label
+                UI_Display_Lbl.Text = UI_Input_Tbx.Text;
+                // Making text box empty
+                UI_Input_Tbx.Text = String.Empty; // or "" empty string
+            }
+            else
+            {
+                // TO TEST THIS UNCOMMENT THIS FOLLOWING
+                /*
+                UI_Input_Tbx.ForeColor = Color.White;
+                UI_Input_Tbx.BackColor = Color.Red;
+
+                // Give a warning message
+                UI_Input_Tbx.Text = " You cannot leave it empty";
+                */
+
+                // Give a pop up message for warning
+                // MessageBox is not a control, it is used to display error/warning messages to user 
+                
+                MessageBox.Show(" You cannot leave text box empty");
+            }
+        }
+
+        private void UI_Sum_Btn_Click(object sender, EventArgs e)
+        {
+            // Grab text from textbox 1 and textbox 2
+            // Perform addition
+            // Show the result on the screen
+
+            int numb1, numb2, sum;
+
+            if (UI_Input1_Tbx.Text.Length > 0 && UI_Input2_Tbx.Text.Length > 0)
+            {
+                int.TryParse(UI_Input1_Tbx.Text, out numb1);
+                int.TryParse(UI_Input2_Tbx.Text, out numb2);
+
+                sum = numb1 + numb2;
+
+                UI_Screen_Lbl.Text = sum.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Please provide both values");
+            }
+        }
+    }
+}
