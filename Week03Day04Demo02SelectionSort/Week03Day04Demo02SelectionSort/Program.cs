44e89972207d3161cbb8bf6513318a8728737299
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2026-06-25 20:32:52 -0600

    add: Week 03 Day 04 Demo Selection Sort
    add: Week 03 Day 04 Demo Selection Sort

---
+++ Week03Day04Demo02SelectionSort/Week03Day04Demo02SelectionSort/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Week03Day04Demo02SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //array to store numbers
+            int[] numbers = { 1000, 7, 0, 9, 21, -37, 17, -1 };
+
+            // Print array before sorting
+            Console.WriteLine("List before sorting");
+            foreach (int numb in numbers)
+            { 
+                Console.WriteLine(numb);
+            }
+
+            //Sort the list using Selection Sort
+            SelectionSort(numbers);
+
+            // Print after sorting
+            Console.WriteLine("List after sorting");
+            foreach (int numb in numbers)
+            {
+                Console.WriteLine(numb);
+            }
+
+        }
+
+        /* Method to perfomr selection  sort
+         * Inputs:  An array to sort
+         * Output: Nothing
+         */
+        public static void SelectionSort(int[] arr)
+        { 
+           int numberOfElements = arr.Length;
+
+            int minElementIndex;
+
+            //Number of passes will be controlled by outer loop
+            for (int i = 0; i < numberOfElements - 1; ++i)
+            {
+                minElementIndex = i;
+                // Finding the smallest element's index from the rest of the list
+                for (int j = i + 1; j < numberOfElements; ++j)
+                {
+                    if (arr[j] < arr[minElementIndex])
+                    {
+                        minElementIndex = j;
+                    }
+                }
+
+                if (minElementIndex != i)
+                {
+                    //Swap the found minimum element 
+                    int temp = arr[minElementIndex];
+                    arr[minElementIndex] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+
+
+        }
+
+    }
+}
