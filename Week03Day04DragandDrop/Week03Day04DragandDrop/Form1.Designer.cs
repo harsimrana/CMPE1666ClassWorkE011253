5d1b0e993432fbe5226d78a60274106e72700c3a
Harsimranjot Aulakh <114257365+harsimrana@users.noreply.github.com>
2026-06-25 19:16:01 -0600

    add: Week 03 Day 04 Drag and Drop

---
+++ Week03Day04DragandDrop/Week03Day04DragandDrop/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace Week03Day04DragandDrop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UI_DisplayData_Tbx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UI_DisplayData_Tbx
+            // 
+            this.UI_DisplayData_Tbx.AllowDrop = true;
+            this.UI_DisplayData_Tbx.Location = new System.Drawing.Point(50, 141);
+            this.UI_DisplayData_Tbx.Multiline = true;
+            this.UI_DisplayData_Tbx.Name = "UI_DisplayData_Tbx";
+            this.UI_DisplayData_Tbx.Size = new System.Drawing.Size(688, 231);
+            this.UI_DisplayData_Tbx.TabIndex = 0;
+            this.UI_DisplayData_Tbx.DragDrop += new System.Windows.Forms.DragEventHandler(this.UI_DisplayData_Tbx_DragDrop);
+            this.UI_DisplayData_Tbx.DragEnter += new System.Windows.Forms.DragEventHandler(this.UI_DisplayData_Tbx_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.UI_DisplayData_Tbx);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UI_DisplayData_Tbx;
+    }
+}
+
---
+++ Week03Day04DragandDrop/Week03Day04DragandDrop/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Week03Day04DragandDrop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Drag Enter event listener: To give user visually impression that this control
+        // will accept drag and drop
+        private void UI_DisplayData_Tbx_DragEnter(object sender, DragEventArgs e)
+        {
+            //e event data object
+            //e.Data , e.Effect, e.x, e.Y
+            //e.Data contains the actual data being dragged
+            // GetDataPresent() checks whether a specific type of data exists inside e.Data
+            //DataFormats.FileDrop means the dragged data should be in file-drop format
+            if ( e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect= DragDropEffects.None;
+            }
+
+        }
+
+        private void UI_DisplayData_Tbx_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                //Getting the path of the file
+
+                /*e.Data.GetData(DataFormats.FileDrop))
+                 *  will return the file paths for files dropped by user
+                 *  Why First() - It takes the first file path from the array
+                 *  FirstorDefault() - gets the first item, but if the collection is empty. 
+                 *  It returns null instead of error
+                 *  Last() - Gets last item
+                 */
+
+                // UNCOMMENT THE FOLLOWING FOR MULTIPLE FILES
+                //string[] filePaths = ((string[])e.Data.GetData(DataFormats.FileDrop));
+
+                //foreach(string singlefilePath in filePaths)
+                //    Console.WriteLine(singlefilePath);
+
+                //
+                string filePath = ((string[])e.Data.GetData(DataFormats.FileDrop)).FirstOrDefault();
+
+                Console.WriteLine(filePath);
+
+                //UI_DisplayData_Tbx.Text = File.ReadAllText(filePath);
+
+                string [] fileLines= File.ReadAllLines(filePath);
+
+                foreach (string line in fileLines)
+                {
+                    UI_DisplayData_Tbx.Text += $"{line} \n";
+                }
+
+
+            }
+            catch (Exception ex)
+            { 
+                Console.WriteLine(e.ToString());    
+            }
+        }
+    }
+}
+
+// Important steps for Cheat Sheet
+/* 1. AllowDrop property for the element on which you are dragging and dropping
+ * 2. Event -  DragEnter :  give visual impression to user that you can drop the file here
+ * 3. Event - DragDrop : main where you will actually read the data from the file(s)
+ * 
+ */
